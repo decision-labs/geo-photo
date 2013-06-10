3a9f2b3fab7c4e174557cf754ff5b03f60f5b22c
Kashif Rasul <kashif.rasul@gmail.com>
2013-06-10 23:17:17 +0200

    use rails 3.2 style migration

--- db/migrate/20130414190510_add_attachment_image_to_photos.rb
+++ db/migrate/20130414190510_add_attachment_image_to_photos.rb
@@ -1,11 +1,5 @@
 class AddAttachmentImageToPhotos < ActiveRecord::Migration
-  def self.up
-    change_table :photos do |t|
-      t.attachment :image
-    end
-  end
-
-  def self.down
-    drop_attached_file :photos, :image
+  def change
+    add_attachment :photos, :image
   end
 end
